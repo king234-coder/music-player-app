96cb54bf4d680e23dd157b89c1bb95e79310bc09
king234-coder <muhammadsalman4674@gmail.com>
2026-07-21 10:58:36 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,147 @@
 let audio = document.getElementById("audio");
 let volume = document.getElementById("volume");
 
-// play/pause
-function playPause() {
-  if (audio.paused) audio.play();
-  else audio.pause();
+let songs = [
+    "Naat 1.mp3",
+    "Naat 2.mp3",
+    "songs/song3.mp3",
+    "songs/song4.mp3",
+    "songs/song5.mp3",
+    "songs/song6.mp3"
+];
+
+let currentSong = 0;
+
+// Play / Pause
+
+function playPause(){
+
+    if(audio.paused){
+        audio.play();
+    }
+    else{
+        audio.pause();
+    }
+
 }
 
-// play selected song
-function playSong(element) {
-  let src = element.getAttribute("data-src");
+// Select song from playlist
 
-  audio.src = src;
-  audio.play();
+function playSong(src, element){
 
-  // extract file name
-  let name = src.split("/").pop().replace(".mp3", "");
+    audio.src = src;
+    audio.play();
 
-  // update now playing
-  document.getElementById("nowPlaying").innerText =
+    currentSong = songs.indexOf(src);
+
+    showSongName(src);
+
+    document.querySelectorAll("#playlist li")
+    .forEach(item=>{
+        item.classList.remove("active");
+    });
+
+    element.classList.add("active");
+
+}
+
+// Next Song
+
+function nextSong(){
+
+    currentSong++;
+
+    if(currentSong >= songs.length){
+        currentSong = 0;
+    }
+
+    loadSong();
+
+}
+
+// Previous Song
+
+function previousSong(){
+
+    currentSong--;
+
+    if(currentSong < 0){
+        currentSong = songs.length - 1;
+    }
+
+    loadSong();
+
+}
+
+// Load song
+
+function loadSong(){
+
+    audio.src = songs[currentSong];
+
+    audio.play();
+
+    showSongName(songs[currentSong]);
+
+    let playlist = document.querySelectorAll("#playlist li");
+
+    playlist.forEach(item=>{
+        item.classList.remove("active");
+    });
+
+    playlist[currentSong].classList.add("active");
+
+}
+
+// Show current song
+
+function showSongName(song){
+
+    let name = song.split("/").pop();
+
+    document.getElementById("nowPlaying").innerText =
     "Now Playing: " + name;
 
-  // active class
-  document.querySelectorAll("#playlist li").forEach(li =>
-    li.classList.remove("active")
-  );
-  element.classList.add("active");
 }
 
-// volume
-volume.oninput = () => {
-  audio.volume = volume.value;
-};
+// Volume
 
-// default
+volume.oninput = function(){
+
+    audio.volume = this.value;
+
+    document.getElementById("volumeValue").innerText =
+    Math.round(this.value * 100) + "%";
+
+}
+
+// Default volume
+
 audio.volume = 0.5;
 volume.value = 0.5;
 
-// AUTO SET NAMES IN PLAYLIST
-document.querySelectorAll("#playlist li").forEach(li => {
-  let src = li.getAttribute("data-src");
-  let name = src.split("/").pop().replace(".mp3", "");
-  li.innerText = name;
-});
+// Auto next song after finish
+
+audio.onended = function(){
+
+    nextSong();
+
+};
+let playButton = document.querySelector(".controls button:nth-child(2)");
+
+function playPause(){
+
+    if(audio.paused){
+
+        audio.play();
+        playButton.innerHTML = "⏸";
+
+    }
+    else{
+
+        audio.pause();
+        playButton.innerHTML = "▶";
+
+    }
+
+}
